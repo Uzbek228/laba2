a79fc838fa12c18086e2807e2b458b0f370bec0e
Мамаджанов Рустам Алишерович <ri67s08@mwp06.csc.local>
2017-05-03 08:54:09 +0700

    Commit2

--- lab2.c
+++ lab2.c
@@ -43,6 +43,6 @@ int main(){
 	  pay=sum+income;
 	  printf("\nSumma v konce sroka: %.2f", pay); 
      }
-     else printf("\nVvedennie dannie ne verny!"); 
+     else print("\nVvedennie dannie ne verny!"); 
      return 0;
 }
